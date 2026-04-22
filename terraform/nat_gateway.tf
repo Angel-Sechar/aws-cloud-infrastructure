6967e0459fd7e9261a4264cc0d1235a060fd1399
Angel Sechar <asv70537592@gmail.com>
2026-04-22 16:47:13 -0500

    fix: apply security hardening and infrastructure improvements v4

--- terraform/nat_gateway.tf
+++ terraform/nat_gateway.tf
@@ -8,14 +8,3 @@ resource "aws_nat_gateway" "main" {
   depends_on    = [aws_internet_gateway.igw]
   tags          = { Name = "nat-backend-application-a" }
 }
-
-resource "aws_eip" "nat_b" {
-  domain = "vpc"
-}
-
-resource "aws_nat_gateway" "nat_b" {
-  allocation_id = aws_eip.nat_b.id
-  subnet_id     = aws_subnet.public_subnet_b.id
-  depends_on    = [aws_internet_gateway.igw]
-  tags          = { Name = "nat-backend-application-b" }
-}
--- terraform/route_tables.tf
+++ terraform/route_tables.tf
@@ -36,17 +36,8 @@ resource "aws_route_table" "private_rt_b" {
   vpc_id = aws_vpc.ecommerce_campus_vpc.id
   route {
     cidr_block     = "0.0.0.0/0"
-    nat_gateway_id = aws_nat_gateway.nat_b.id
+    nat_gateway_id = aws_nat_gateway.main.id
   }
   tags = { Name = "rtb-private-b" }
 }
 
-resource "aws_route_table_association" "private_a" {
-  subnet_id      = aws_subnet.private_subnet_a.id
-  route_table_id = aws_route_table.private_rt_a.id
-}
-
-resource "aws_route_table_association" "private_b" {
-  subnet_id      = aws_subnet.private_subnet_b.id
-  route_table_id = aws_route_table.private_rt_b.id
-}
